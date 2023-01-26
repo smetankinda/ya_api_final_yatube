9327e922f6d0f9b022b27da99713432c0cb3b543
Dmitry Smetankin <smetankinda@gmail.com>
2023-01-26 19:49:00 +0300

    Написание проекта

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -24,7 +24,6 @@ v1_router.register(
 )
 v1_router.register('follow', FollowViewSet, basename='follow')
 
-
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
     path('v1/', include('djoser.urls')),
